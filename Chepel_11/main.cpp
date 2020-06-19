0ef9487d013bc5a44fef444e4982b0d91c4c671d
sampletext32 <sampletext32@bk.ru>
2020-06-19 03:24:08 +0300

    Add project

---
+++ Chepel_11/main.cpp
@@ -0,0 +1,43 @@
+﻿#include <iomanip>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string last_word(string& line)
+{
+	string last;
+	int index = -1;
+	for (int i = line.length() - 1; i >= 0; --i)
+	{
+		if (line[i] == ' ')
+		{
+			index = i + 1;
+			break;
+		}
+	}
+	for (int i = index; i < line.length(); ++i)
+	{
+		last += line[i];
+	}
+	return last;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+
+	cout << "Автор: Чепель Егор Максимович" << endl;
+	cout << "Программа для поиска последнего слова в строке" << endl;
+
+	string line;
+	cout << "Введите строку: ";
+	getline(cin, line);
+
+	string last = last_word(line);
+	cout << "Последнее слово: " << last << "\n";
+
+	system("pause");
+
+	return 0;
+}
